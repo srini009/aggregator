48712435deb7d1219572291716255461eb3ec2b1
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-03-09 00:41:29 -0500

    Add some printfs

--- bin/server.c
+++ bin/server.c
@@ -43,13 +43,11 @@ int main(int argc, char** argv)
     // Exchange addresses
     char * addresses_buf = (char*)malloc(1024*size);
     int ret = MPI_Gather(addr, addr_str_size-1, MPI_BYTE, addresses_buf, 1024, MPI_BYTE, 0, MPI_COMM_WORLD);
-    if(ret == MPI_SUCCESS) 
-        fprintf(stderr, "MPI returned successfully\n");
  
     margo_addr_free(mid, my_address);
     if(!rank) {
            //fprintf(stderr, "Server running at address %s, with provider id 42", addr_str);
-         fprintf(fp, "%s\n", addresses_buf);
+         fprintf(fp, "%s\n", &addresses_buf[18]);
     }
 
     fclose(fp);
